315b9fba95d71c38f9ec983b9d269d053b8cc21b
lynnug <lynnasiimwe@gmail.com>
2014-11-20 18:27:28 +0300

    adding entity framework

---
+++ LearnCSharp/Models/Actor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp.Models
+{
+    public class Actor
+    {
+           [Key]
+           public int Id { get; set; }
+           [Required]
+           [StringLength(50)]
+           public String Name { get; set; }
+           [Required]
+           [StringLength(50)]
+           public String Surname { get; set; }
+           [StringLength(200)]
+           public String Note { get; set; }
+    }
+}
--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using LearnCSharp.Models;
 
 namespace LearnCSharp
 {
@@ -76,6 +76,95 @@ namespace LearnCSharp
             Console.WriteLine(pi);
             Console.ReadKey();*/
 
+            using (DvdContext db = new DvdContext())
+            {
+               
+                // film generes
+                FilmGenere actionGenere = db.FilmGeneres.Where
+                (g => g.Name == "Action").SingleOrDefault();
+                FilmGenere scifiGenere = db.FilmGeneres.Where
+                (g => g.Name == "SciFi").SingleOrDefault();
+                // find the producer
+                Producer jjAbrams = db.Producers.Where
+                (p => p.FullName == "J.J. Abrams").SingleOrDefault();
+                // we found the producer
+                if (jjAbrams != null)
+                {
+                    // add some films to that producer
+                    FilmTitle film1 = new FilmTitle()
+                    {
+                        Title = "Mission: Impossible III",
+                        ReleaseDate=DateTime.Now,
+                        Duration = 126,
+                        Story = "Ethan Hunt comes face to face with a dangerous and ...",
+                        FilmGenere = actionGenere
+                    };
+                    film1.Producers = new List<Producer>();
+                    film1.Producers.Add(jjAbrams);
+                    db.FilmTitles.Add(film1);
+                    FilmTitle film2 = new FilmTitle()
+                    {
+                        Title = "Star Trek Into Darkness",
+                        ReleaseDate=DateTime.Now,
+                        Duration = 132,
+                        Story = "After the crew of the" 
+                        +" find an unstoppable force  ...",
+                        FilmGenere = scifiGenere
+                    };
+                    film2.Producers = new List<Producer>();
+                    film2.Producers.Add(jjAbrams);
+                    db.FilmTitles.Add(film2);
+                    // add some film roles
+                    Role leadRole = db.Roles.Where(r => 
+                    r.Name == "Lead").SingleOrDefault();
+                    Role supportingRole = db.Roles.Where(r => 
+                    r.Name == "Supporting").SingleOrDefault();
+                    // load the actors
+                    Actor tom = db.Actors.Where(a => 
+                    a.Surname == "Cruise").SingleOrDefault();
+                    Actor quinto = db.Actors.Where(a => 
+                    a.Surname == "Quinto").SingleOrDefault();
+                    Actor pine = db.Actors.Where(a => 
+                    a.Surname == "Pine").SingleOrDefault();
+                    // add filmroles
+                    db.FilmActorRoles.Add(new FilmActorRole()
+                    {
+                        Actor = tom,
+                        Role = leadRole,
+                        FilmTitle = film1,
+                        Character = "Ethan",
+                        Description = "Ethan Hunt comes face to face with" 
+                        +"a dangerous and sadistic arms dealer while trying to "
+                        +"keep his identity secret in order to protect his girlfriend."
+                     });
+                    db.FilmActorRoles.Add(new FilmActorRole()
+                    {
+                         Actor = pine,
+                         Role = leadRole,
+                         FilmTitle = film2,
+                         Character = "Kirk",
+                         Description = "Captain Kirk"
+                    });
+                   db.FilmActorRoles.Add(new FilmActorRole()
+                   {
+                          Actor = quinto,
+                          Role = supportingRole,
+                          FilmTitle = film2,
+                          Character = "Spock",
+                         Description = "Spock was born in 2230, "
+                         +"n the city of Shi'Kahr on the planet Vulcan"
+                    });
+              }
+             // save data to db
+             db.SaveChanges();
+             var Actors = db.FilmActorRoles.ToList();
+             foreach (var aActor in Actors)
+             {
+                  Console.WriteLine(aActor.Description);
+             }
+                Console.ReadKey();
+            }
+            
         }
         
         
