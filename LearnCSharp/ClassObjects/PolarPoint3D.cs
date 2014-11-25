1788080f2d8ea97047a5adaa0ff8d71ddc83a640
lynnug <lynnasiimwe@gmail.com>
2014-11-25 16:12:31 +0300

    adding small fix to naming

---
+++ LearnCSharp/ClassObjects/PolarPoint3D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    struct PolarPoint3D
+    {
+        //prevent code duplication
+        public PolarPoint3D(double distance,double angle)
+            :this(distance,angle,0)
+        {
+
+        }
+        public PolarPoint3D(double distance,double angle, double altitude)
+            :this()
+        {
+            Distance = distance;
+            Angle = angle;
+            Altitude = altitude;
+        }
+        public double Distance
+        {
+            get;
+            private set;
+        }
+        public double Angle
+        {
+            get;
+            private set;
+        }
+
+        public double Altitude
+        {
+            get;
+            private set;
+        }
+    }
+}
